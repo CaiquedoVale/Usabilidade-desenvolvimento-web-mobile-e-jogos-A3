e956ab09be93a5843ddd5d6aca0d1ea3712251d6
Juliano Rafael de Souza <86811783+JulianodeSouza@users.noreply.github.com>
2025-10-19 20:28:38 -0300

    wip: Event page

---
+++ app/pages/pageNotFound/index.tsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <section className="container-page-not-found">
+      <img src="/assets/page-not-found.png" alt="Page Not Found" />
+    </section>
+  );
+}
